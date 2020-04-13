04ff1689e5c3cb86dee638f6d4a64a877efb5e44
Jaden Costa <31477326+Qflat@users.noreply.github.com>
2020-04-13 19:39:36 -0400

    Add files via upload
    Added process(request) method
    
    Not sure how to gather the data from form/home.html. I get a 404 error when the submit button is run. Not sure if I need to add a url extension in urls.py or what.

--- form/views.py
+++ form/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'form/home.html')
+
+def process(request):
+    if request.method=='POST':
+        print(request.session['toe'])
