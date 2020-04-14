a33ed663ae5e6841ca05bbf297b590e51ef0904c
Jaden Costa <31477326+Qflat@users.noreply.github.com>
2020-04-13 22:32:29 -0400

    Add files via upload

--- form/urls.py
+++ form/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('', views.index, name='index')]
+    url('', views.index, name='index'),
+    url('process', views.process, name='process')]
