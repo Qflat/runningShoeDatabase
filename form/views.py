bf8715d81dfe603a732e45a271831a8e74997d42
Jaden Costa <31477326+Qflat@users.noreply.github.com>
2020-04-20 21:40:34 -0400

    Fixed potential bug
    Line 16

--- form/views.py
+++ form/views.py
@@ -13,23 +13,9 @@ class ShoeForm(TemplateView):
 		return render(request, self.template_name, {'form':form})
 
 	def post(self, request):
-		form=ShoeForm(request.POST)
+		form=models.ShoeForm(request.POST)
 		if form.is_valid():
-			brand=form.cleaned_data['Shoe_Brand']
+			brand=form.cleaned_data['shoe_brand']
+			print(brand)
 		args={'form':form,'text':brand}
 		return render(request, self.template_name, args)
-
-
-
-def index(request):
-	if request.method=='POST':
-		form=ShoeForm(request.POST)
-		if form.is_valid():
-			'''
-			Process data
-			'''
-			return HttpResponseRedirect('/meetDeveloper/')
-	else:
-		form=ShoeForm()
-
-	return render(request, 'form/home.html', {'form':form})
