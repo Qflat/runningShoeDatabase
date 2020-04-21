595344a35ed21a5d79753f27394e7037c9bf5541
Jaden Costa <31477326+Qflat@users.noreply.github.com>
2020-04-20 21:24:24 -0400

    Upgraded models.py
    Better labeling

--- form/models.py
+++ form/models.py
@@ -2,11 +2,11 @@ from django import forms
 
 # Create your models here.
 class ShoeForm(forms.Form):
-	Shoe_Brand=forms.CharField()
-	Shoe_Name=forms.CharField()
-	Shoe_Heel=forms.CharField()
-	Shoe_Toes=forms.CharField()
-	Shoe_Type=forms.CharField()
-	Shoe_Spikes=forms.CharField()
-	Shoe_Event=forms.CharField()
-	Shoe_Comments=forms.CharField(required=False)
+	shoe_brand=forms.CharField(label='Shoe Brand')
+	shoe_name=forms.CharField(label='Name of Shoe Model')
+	heel_stack=forms.CharField(label='Heel Stack Height (mm)')
+	toe_stack=forms.CharField(label='Toe Stack Height (mm)')
+	shoe_type=forms.CharField(label='Is this shoe a trainer, spikes, or racing flat?')
+	number_of_spikes=forms.CharField(label='How many spikes can this shoe hold?')
+	event=forms.CharField(label='What event would these shoes be best for?')
+	comments=forms.CharField(required=False, label='Please type any other comments here')
